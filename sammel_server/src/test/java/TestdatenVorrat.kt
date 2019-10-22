3684bd6caaae09b6c8c4b91cd5828dcf00ecc21f
BWXW035 <ubihvsl11>
2019-10-22 16:57:29 +0200

    Unit-Tests für Server nachziehen #8 - Unit-Tests für TerminRestResource

--- sammel_server/src/test/java/TestdatenVorrat.kt
+++ sammel_server/src/test/java/TestdatenVorrat.kt
@@ -1,5 +1,8 @@
 import database.stammdaten.Ort
 import database.benutzer.Benutzer
+import database.termine.Termin
+import rest.TermineRestResource
+import java.time.LocalDateTime
 
 class TestdatenVorrat {
     companion object {
@@ -29,6 +32,32 @@ class TestdatenVorrat {
 
         fun rosa(): Benutzer {
             return Benutzer(1, "Rosa Luxemburg", "Ich bin, ich war ich, werde sein", null)
+        ***REMOVED***
+
+        fun terminOhneTeilnehmer(): Termin {
+            return Termin(1,
+                    LocalDateTime.of(2019, 10, 22, 16, 30, 0),
+                    LocalDateTime.of(2019, 10, 22, 18, 0, 0),
+                    nordkiez(),
+                    sammeltermin(),
+                    emptyList())
+        ***REMOVED***
+
+        fun terminMitTeilnehmer(): Termin {
+            return Termin(2,
+                    LocalDateTime.of(2019, 10, 22, 12, 0, 0),
+                    LocalDateTime.of(2019, 10, 22, 15, 0, 0),
+                    goerli(),
+                    sammeltermin(),
+                    listOf(karl(), rosa()))
+        ***REMOVED***
+
+        fun terminDtoMitTeilnehmer(): TermineRestResource.TerminDto {
+            return TermineRestResource.TerminDto.convertFromTermin(terminMitTeilnehmer())
+        ***REMOVED***
+
+        fun terminDtoOhneTeilnehmer(): TermineRestResource.TerminDto {
+            return TermineRestResource.TerminDto.convertFromTermin(terminOhneTeilnehmer())
         ***REMOVED***
     ***REMOVED***
 ***REMOVED***
