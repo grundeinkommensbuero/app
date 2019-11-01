848fff0267ace1cdebbfd6fb33bfe7816b6798c2
BWXW035 <ubihvsl11>
2019-11-01 23:57:46 +0100

    UI truthy test für TerminCard

---
+++ sammel_app/test/routes/TerminCard_ui_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sammel_app/model/Termin.dart';
+import 'package:sammel_app/routes/TerminCard.dart';
+import 'TerminCard_test.dart';
+
+void main() {
+  testWidgets('TerminCard startet fehlerfrei', (WidgetTester tester) async {
+    var termin = Termin(
+        0, DateTime.now(), DateTime.now(), nordkiez(), 'Sammel-Termin', []);
+
+    await tester.pumpWidget(MaterialApp(home: TerminCard(termin)));
+
+    expect(find.text(termin.typ), findsOneWidget);
+    expect(find.text(TerminCard.erzeugeOrtText(termin.ort)), findsOneWidget);
+    expect(find.text(TerminCard.erzeugeDatumText(termin.beginn, termin.ende)),
+        findsOneWidget);
+  ***REMOVED***);
+***REMOVED***
