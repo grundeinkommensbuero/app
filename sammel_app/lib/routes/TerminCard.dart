7d1e14eef25b8315d0c6be9ecb2524760e4ebeca
BWXW035 <ubihvsl11>
2019-11-01 17:42:24 +0100

    Unit-Tests für TermineCard

--- sammel_app/lib/routes/TerminCard.dart
+++ sammel_app/lib/routes/TerminCard.dart
@@ -1,6 +1,7 @@
 import 'package:date_format/date_format.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sammel_app/model/Ort.dart';
 import 'package:sammel_app/model/Termin.dart';
 import 'package:sammel_app/services/Benutzer.dart';
 import 'package:sammel_app/shared/DateTimeHelfer.dart';
@@ -8,7 +9,8 @@ import 'package:sammel_app/shared/DateTimeHelfer.dart';
 class TerminCard extends StatelessWidget {
   final Termin termin;
   static const _PADDING = 5.0;
-  static const _HELLGELB = Color.fromARGB(255, 250, 250, 150);
+  static const DUNKELGELB = Colors.amberAccent;
+  static const HELLGELB = Color.fromARGB(255, 250, 250, 150);
   static final TextStyle style = TextStyle(
     color: Color.fromARGB(255, 129, 28, 98),
     fontSize: 15.0,
@@ -20,9 +22,7 @@ class TerminCard extends StatelessWidget {
     return Container(
         child: Column(children: [
       RaisedButton(
-        color: termin.ende.isAfter(DateTime.now())
-            ? Colors.amberAccent
-            : _HELLGELB,
+        color: faerbeVergangeneTermine(termin.ende),
         child: Column(children: [
           Text('${termin.typ***REMOVED***',
               style: TextStyle(
@@ -42,16 +42,12 @@ class TerminCard extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                   Text(
-                    '${termin.ort.bezirk***REMOVED***, ${termin.ort.ort***REMOVED***',
+                    erzeugeOrtText(termin.ort),
                     style: style,
                     overflow: TextOverflow.clip,
                   ),
-                  erzeugeDatumZeile(termin.beginn, termin.ende),
-                  Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: termin.teilnehmer
-                          .map((benutzer) => erzeugeTeilnehmerZeile(benutzer))
-                          .toList()),
+                  Text(erzeugeDatumText(termin.beginn, termin.ende)),
+                  erzeugeTeilnehmerRow(termin.teilnehmer),
                 ])),
           ]),
         ]),
@@ -63,6 +59,27 @@ class TerminCard extends StatelessWidget {
     ]));
   ***REMOVED***
 
+  static Row erzeugeTeilnehmerRow(List<Benutzer> teilnehmer) {
+    return Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: teilnehmer
+            .map((benutzer) => Row(
+                  children: <Widget>[
+                    Icon(Icons.supervised_user_circle),
+                    Text(' ${benutzer.name***REMOVED*** '),
+                  ],
+                ))
+            .toList());
+  ***REMOVED***
+
+  static String erzeugeOrtText(Ort ort) {
+    return '${ort.bezirk***REMOVED***, ${ort.ort***REMOVED***';
+  ***REMOVED***
+
+  static Color faerbeVergangeneTermine(DateTime ende) {
+    return ende.isAfter(DateTime.now()) ? DUNKELGELB : HELLGELB;
+  ***REMOVED***
+
   Widget erzeugeTeilnehmerZeile(Benutzer benutzer) {
     return Row(
       children: <Widget>[
@@ -72,7 +89,16 @@ class TerminCard extends StatelessWidget {
     );
   ***REMOVED***
 
-  Widget erzeugeDatumZeile(DateTime beginn, DateTime ende) {
+  static String erzeugeDatumText(DateTime beginn, DateTime ende) {
+    return ''
+        '${ermittlePrefix(beginn)***REMOVED***'
+        '${formatDate(beginn, [dd, '.', mm, '.'])***REMOVED*** '
+        'um ${formatDate(beginn, [HH, ':', nn])***REMOVED*** Uhr, '
+        '${ende.difference(beginn).inHours***REMOVED*** Stunde'
+        '${ende.difference(beginn).inHours > 1 ? 'n' : ''***REMOVED***';
+  ***REMOVED***
+
+  static String ermittlePrefix(DateTime beginn) {
     DateTime heuteNacht = DateTime(DateTime.now().year, DateTime.now().month,
         DateTime.now().day, 23, 59, 59);
     String prefix = '';
@@ -87,10 +113,6 @@ class TerminCard extends StatelessWidget {
         ***REMOVED***
       ***REMOVED***
     ***REMOVED***
-    return Text(''
-        '$prefix'
-        '${formatDate(beginn, [dd, '.', mm])***REMOVED*** '
-        'um ${formatDate(beginn, [HH, ':', nn])***REMOVED*** Uhr, '
-        '${ende.difference(beginn).inHours***REMOVED*** Stunden');
+    return prefix;
   ***REMOVED***
 ***REMOVED***
---
+++ sammel_app/test/routes/TerminCard_test.dart
@@ -0,0 +1,118 @@
+import 'package:sammel_app/model/Ort.dart';
+import 'package:sammel_app/routes/TerminCard.dart';
+import 'package:sammel_app/services/Benutzer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('erzeugeDatumZeile berechnet Dauer', () {
+    test('korrekt', () {
+      var datumText = TerminCard.erzeugeDatumText(
+          DateTime(2019, 10, 30, 20), DateTime(2019, 10, 30, 23));
+
+      expect(datumText.substring(datumText.lastIndexOf(',') + 2), '3 Stunden');
+    ***REMOVED***);
+
+    test('und rundet ab', () {
+      var datumText = TerminCard.erzeugeDatumText(
+          DateTime(2019, 10, 30, 20, 55), DateTime(2019, 10, 30, 23, 54));
+
+      expect(datumText.substring(datumText.lastIndexOf(',') + 2), '2 Stunden');
+    ***REMOVED***);
+
+    test('und singularisiert bei einer Stunde', () {
+      var datumText = TerminCard.erzeugeDatumText(
+          DateTime(2019, 10, 30, 20), DateTime(2019, 10, 30, 21));
+
+      expect(datumText.substring(datumText.lastIndexOf(',') + 2), '1 Stunde');
+    ***REMOVED***);
+  ***REMOVED***);
+  group('ermittlePrefix', () {
+    test('ermittelt Heute richtig', () {
+      var heute = DateTime(now().year, now().month, now().day, 20);
+      var datumText = TerminCard.ermittlePrefix(heute);
+
+      expect(datumText.substring(0, datumText.indexOf(',')), 'Heute');
+    ***REMOVED***);
+
+    test('ermittelt Morgen richtig', () {
+      var morgen = DateTime(now().year, now().month, now().day, 20)
+          .add(Duration(days: 1));
+      var datumText = TerminCard.ermittlePrefix(morgen);
+
+      expect(datumText.substring(0, datumText.indexOf(',')), 'Morgen');
+    ***REMOVED***);
+
+    test('ermittelt Wochentag richtig', () {
+      var spaeter = DateTime(now().year, now().month, now().day, 20)
+          .add(Duration(days: 2));
+      var datumText = TerminCard.ermittlePrefix(spaeter);
+
+      expect(datumText.substring(0, datumText.indexOf(',')), 'Sonntag');
+    ***REMOVED***);
+  ***REMOVED***);
+  group('faerbeVergangeneTermine', () {
+    test('faerbt vergangene Termine hellgelb', () {
+      DateTime gestern = DateTime.now().subtract(Duration(days: 1));
+      expect(TerminCard.faerbeVergangeneTermine(gestern),
+          equals(TerminCard.HELLGELB));
+
+      DateTime vor1Stunde = DateTime.now().subtract(Duration(hours: 1));
+      expect(TerminCard.faerbeVergangeneTermine(vor1Stunde),
+          equals(TerminCard.HELLGELB));
+
+      DateTime vor1Minute = DateTime.now().subtract(Duration(minutes: 1));
+      expect(TerminCard.faerbeVergangeneTermine(vor1Minute),
+          equals(TerminCard.HELLGELB));
+    ***REMOVED***);
+
+    test('faerbt zukünftige Termine dunkelgelb', () {
+      DateTime morgen = DateTime.now().add(Duration(days: 1));
+      expect(TerminCard.faerbeVergangeneTermine(morgen),
+          equals(TerminCard.DUNKELGELB));
+
+      DateTime in1Stunde = DateTime.now().add(Duration(hours: 1));
+      expect(TerminCard.faerbeVergangeneTermine(in1Stunde),
+          equals(TerminCard.DUNKELGELB));
+
+      DateTime in1Minute = DateTime.now().add(Duration(minutes: 1));
+      expect(TerminCard.faerbeVergangeneTermine(in1Minute),
+          equals(TerminCard.DUNKELGELB));
+    ***REMOVED***);
+  ***REMOVED***);
+  group('erzeugeOrtText', () {
+    test('konkateniert Bezirk und Ort', () {
+      var text = TerminCard.erzeugeOrtText(nordkiez());
+
+      expect(text, 'Friedrichshain-Kreuzberg, Friedrichshain Nordkiez');
+    ***REMOVED***);
+  ***REMOVED***);
+  group('erzeugeTeilnehmerRow', () {
+    test('erzeugt leere Zeile bei keinen Teilnehmern', () {
+      List<Benutzer> teilnehmer = [];
+      var row = TerminCard.erzeugeTeilnehmerRow(teilnehmer);
+
+      expect(row.children.length, 0);
+    ***REMOVED***);
+
+    test('erzeugt korrekte Zeile bei einem Teilnehmer', () {
+      List<Benutzer> teilnehmer = [rosa()];
+      var row = TerminCard.erzeugeTeilnehmerRow(teilnehmer);
+
+      expect(row.children.length, 1);
+    ***REMOVED***);
+
+    test('erzeugt korrekte Zeile bei einem Teilnehmer', () {
+      List<Benutzer> teilnehmer = [karl(), rosa()];
+      var row = TerminCard.erzeugeTeilnehmerRow(teilnehmer);
+
+      expect(row.children.length, 2);
+    ***REMOVED***);
+  ***REMOVED***);
+***REMOVED***
+
+DateTime now() => DateTime.now();
+
+Ort nordkiez() => Ort(0, "Friedrichshain-Kreuzberg", "Friedrichshain Nordkiez");
+
+Benutzer rosa() => Benutzer('Rosa Luxemburg');
+Benutzer karl() => Benutzer('Karl Marx', '123456789');
