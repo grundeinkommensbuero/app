ae9b65f450763808f094ef4d4efccc46dd2aa2ca
kybernetik <chemo@mailbox.org>
2019-12-07 21:51:40 +0100

    Filter in GUI einbauen #23 - Tage-Auswahl ausgelagert - Zeitraum-Auswahl ausgelagert - Ort-Auswahl ausgelagert - Ort-Auswahl um singuläre Auswahl erweitert

---
+++ sammel_app/lib/shared/showLocationPicker.dart
@@ -0,0 +1,191 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:sammel_app/model/Ort.dart';
+
+Future<List<Ort>> showMultipleLocationPicker(
+    BuildContext context, final List<Ort> previousLocations, final allLocations,
+    {bool multiple = false}) async {
+  List<BezirkItem> districs =
+      erzeugeBezirkDaten(List.from(previousLocations), allLocations);
+  await showDialog<List<int>>(
+      context: context,
+      builder: (context) => StatefulBuilder(builder: (context, setDialogState) {
+            List<int> selectedLocations =
+                previousLocations?.map((ort) => ort.id).toList();
+            if (selectedLocations == null)
+              selectedLocations = []; // Null-Sicherheit
+
+            return SimpleDialog(
+                title: multiple
+                    ? const Text('Wähle einen oder mehrere Orte')
+                    : const Text('Wähle einen Ort'),
+                children: <Widget>[
+                  ExpansionPanelList(
+                      expansionCallback: (int index, bool expanded) {
+                        setDialogState(() {
+                          districs[index].expanded = !expanded;
+                        });
+                      },
+                      children: expansionPanelListe(
+                          context,
+                          allLocations,
+                          selectedLocations,
+                          districs,
+                          setDialogState,
+                          multiple)),
+                  multiple
+                      ? RaisedButton(
+                          child: Text('Fertig'),
+                          onPressed: () => Navigator.pop(context))
+                      : RaisedButton(
+                          child: Text('Keine Auswahl'),
+                          onPressed: () {
+                            eraseSelections(districs);
+                            Navigator.pop(context);
+                          },
+                        )
+                ]);
+          }));
+  return districs
+      .expand((bezirk) => bezirk.ortAuswahl.entries)
+      .where((entry) => entry.value)
+      .map((entry) => entry.key)
+      .toList();
+}
+
+List<BezirkItem> erzeugeBezirkDaten(
+    List<Ort> selectedLocations, List<Ort> allLocations) {
+  var bezirkNamen = allLocations.map((ort) => ort.bezirk).toSet();
+  return bezirkNamen
+      .map((name) => BezirkItem(
+          name,
+          Map<Ort, bool>()
+            ..addEntries(allLocations.where((ort) => ort.bezirk == name).map(
+                (ort) => MapEntry(
+                    ort,
+                    selectedLocations
+                        .any((filterOrt) => filterOrt.id == ort.id)))),
+          false))
+      .toList();
+}
+
+List<ExpansionPanel> expansionPanelListe(
+    BuildContext context,
+    List<Ort> orte,
+    List<int> ausgewOrte,
+    List<BezirkItem> districts,
+    Function setdialogState,
+    bool multiple) {
+  return districts
+      .map((item) => expansionPanel(
+          context, item, orte, ausgewOrte, setdialogState, districts, multiple))
+      .toList();
+}
+
+ExpansionPanel expansionPanel(
+    BuildContext context,
+    BezirkItem disctrict,
+    List<Ort> locations,
+    List<int> selLoc,
+    Function setDialogState,
+    List<BezirkItem> districts,
+    bool multiple) {
+  return ExpansionPanel(
+      headerBuilder: (BuildContext context, bool isExpanded) => multiple
+          ? disctrictCheckbox(disctrict, locations, selLoc, setDialogState)
+          : disctrictPanel(disctrict, locations, selLoc, setDialogState),
+      canTapOnHeader: true,
+      isExpanded: disctrict.expanded,
+      body: Column(
+          children: disctrict.ortAuswahl.keys
+              .map((ort) => multiple
+                  ? locationCheckbox(ort, disctrict, setDialogState)
+                  : locationButton(context, ort, disctrict, districts))
+              .toList()));
+}
+
+ListTile disctrictPanel(BezirkItem item, List<Ort> orte,
+    List<int> selectedLocations, Function setDialogState) {
+  return ListTile(
+    title: Text(item.bezirk),
+  );
+}
+
+CheckboxListTile disctrictCheckbox(BezirkItem item, List<Ort> locations,
+    List<int> ausgewOrte, Function setDialogState) {
+  return CheckboxListTile(
+    value: item.ortAuswahl.values.every((ausgewaehlt) => ausgewaehlt == true),
+    title: Text(item.bezirk),
+    onChanged: (bool ausgewaehlt) {
+      setDialogState(() {
+        if (ausgewaehlt) {
+          item.ortAuswahl.keys.forEach((ort) => item.ortAuswahl[ort] = true);
+        } else {
+          item.ortAuswahl.keys.forEach((ort) => item.ortAuswahl[ort] = false);
+        }
+      });
+    },
+  );
+}
+
+bool alleOrteDesBezirksAusgewaehlt(
+    String bezirk, List<Ort> orte, List<int> ausgewOrte) {
+  return orte.every((ortStammdaten) =>
+      ortStammdaten.bezirk != bezirk || ausgewOrte.contains(ortStammdaten.id));
+}
+
+CheckboxListTile locationCheckbox(Ort ort, BezirkItem bezirk, setDialogState) {
+  return CheckboxListTile(
+    value: bezirk.ortAuswahl[ort],
+    title: Text('      ' + ort.ort),
+    onChanged: (bool wurdeAusgewaehlt) {
+      setDialogState(() => bezirk.ortAuswahl[ort] = wurdeAusgewaehlt);
+    },
+  );
+}
+
+ListTile locationButton(BuildContext context, Ort location, BezirkItem district,
+    List<BezirkItem> districts) {
+  return ListTile(
+    title: FlatButton(
+        padding: EdgeInsets.zero,
+        child: Text('      ' + location.ort),
+        textTheme: district.ortAuswahl[location]
+            ? ButtonTextTheme.accent
+            : ButtonTextTheme.normal,
+        onPressed: () {
+          eraseSelections(districts);
+          district.ortAuswahl[location] = true;
+          return Navigator.pop(context);
+        }),
+  );
+}
+
+void eraseSelections(List<BezirkItem> districts) {
+  districts.forEach((district) => district.ortAuswahl.keys
+      .forEach((location) => district.ortAuswahl[location] = false));
+}
+
+class BezirkItem {
+  String bezirk;
+  Map<Ort, bool> ortAuswahl;
+  bool expanded;
+
+  BezirkItem(this.bezirk, this.ortAuswahl, this.expanded);
+
+  @override
+  String toString() {
+    return bezirk +
+        ':' +
+        ortAuswahl.entries
+            .map((entry) =>
+                jsonEncode(entry.key) + ' (' + entry.value.toString() + ') ')
+            .toList()
+            .toString() +
+        ' (' +
+        expanded.toString() +
+        ')';
+  }
+}
---
+++ sammel_app/lib/shared/showMultipleDatePicker.dart
@@ -0,0 +1,87 @@
+import 'package:calendarro/calendarro.dart';
+import 'package:calendarro/default_weekday_labels_row.dart';
+import 'package:flutter/material.dart';
+import 'package:jiffy/jiffy.dart';
+
+import 'ChronoHelfer.dart';
+
+Future<List<DateTime>> showMultipleDatePicker(
+    List<DateTime> previousSelectedDates, BuildContext context) async {
+  DateTime currentMonth = DateTime.now();
+  List<DateTime> selectedDatesFromFilter = []..addAll(previousSelectedDates);
+  if (selectedDatesFromFilter == null) selectedDatesFromFilter = [];
+  var selectedDatesFromDialog = await showDialog<List<DateTime>>(
+      context: context,
+      builder: (context) => StatefulBuilder(builder: (context, setDialogState) {
+            return SimpleDialog(
+                titlePadding:
+                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 0.0),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    RaisedButton(
+                      shape: CircleBorder(),
+                      child: Icon(Icons.arrow_left),
+                      onPressed: () => setDialogState(() => currentMonth =
+                          Jiffy(currentMonth).subtract(months: 1)),
+                    ),
+                    Text(
+                      ChronoHelfer.monthName(currentMonth.month) +
+                          '\n' +
+                          currentMonth.year.toString(),
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                      textWidthBasis: TextWidthBasis.parent,
+                    ),
+                    RaisedButton(
+                      shape: CircleBorder(),
+                      child: Icon(Icons.arrow_right),
+                      onPressed: () => setDialogState(() =>
+                          currentMonth = Jiffy(currentMonth).add(months: 1)),
+                    ),
+                  ],
+                ),
+                children: <Widget>[
+                  Container(
+                      height: 260.0,
+                      width: 1.0,
+                      child: Calendarro(
+                        startDate: Jiffy(currentMonth).startOf("month"),
+                        endDate: Jiffy(currentMonth).endOf("month"),
+                        selectedDates: selectedDatesFromFilter,
+                        weekdayLabelsRow: GerCalendarroWeekdayLabelsView(),
+                        selectionMode: SelectionMode.MULTI,
+                        displayMode: DisplayMode.MONTHS,
+                      )),
+                  ButtonBar(alignment: MainAxisAlignment.center, children: [
+                    RaisedButton(
+                      child: Text("Keine"),
+                      onPressed: () => Navigator.pop(context, <DateTime>[]),
+                    ),
+                    RaisedButton(
+                      child: Text("Auswählen"),
+                      onPressed: () =>
+                          Navigator.pop(context, selectedDatesFromFilter),
+                    )
+                  ])
+                ]);
+          }));
+  return selectedDatesFromDialog;
+}
+
+class GerCalendarroWeekdayLabelsView extends CalendarroWeekdayLabelsView {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(child: Text("Mo", textAlign: TextAlign.center)),
+        Expanded(child: Text("Di", textAlign: TextAlign.center)),
+        Expanded(child: Text("Mi", textAlign: TextAlign.center)),
+        Expanded(child: Text("Do", textAlign: TextAlign.center)),
+        Expanded(child: Text("Fr", textAlign: TextAlign.center)),
+        Expanded(child: Text("Sa", textAlign: TextAlign.center)),
+        Expanded(child: Text("So", textAlign: TextAlign.center)),
+      ],
+    );
+  }
+}
