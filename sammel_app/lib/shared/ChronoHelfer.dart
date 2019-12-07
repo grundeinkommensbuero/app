224984d0b47790d62bf9aae09d3ef155976109b4
kybernetik <chemo@mailbox.org>
2019-12-07 18:34:24 +0100

    Filter in GUI einbauen #23 - Tage-Auswahl Zwischenstand

--- sammel_app/lib/shared/ChronoHelfer.dart
+++ sammel_app/lib/shared/ChronoHelfer.dart
@@ -22,6 +22,36 @@ class ChronoHelfer {
     throw Exception('Unbekannter Wochentag');
   ***REMOVED***
 
+  static String monthName(int month) {
+    switch (month) {
+      case 1:
+        return 'Januar';
+      case 2:
+        return 'Februar';
+      case 3:
+        return 'März';
+      case 4:
+        return 'April';
+      case 5:
+        return 'Mai';
+      case 6:
+        return 'Juni';
+      case 7:
+        return 'Juli';
+      case 8:
+        return 'August';
+      case 9:
+        return 'September';
+      case 10:
+        return 'Oktober';
+      case 11:
+        return 'November';
+      case 12:
+        return 'Dezember';
+    ***REMOVED***
+    throw Exception('Unbekannter Monat');
+  ***REMOVED***
+
   static DateFormat dateFormatHHmmss = DateFormat("HH:mm:ss");
   static DateFormat dateFormatHHmm = DateFormat("HH:mm");
 
