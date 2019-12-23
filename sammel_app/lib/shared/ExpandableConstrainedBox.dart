18bf43c2eb9f2a43320dee25ebf4794e98d2d481
kybernetik <chemo@mailbox.org>
2019-12-23 18:13:36 +0100

    aufgeräumt

--- sammel_app/lib/shared/ExpandableConstrainedBox.dart
+++ sammel_app/lib/shared/ExpandableConstrainedBox.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:sammel_app/shared/DweTheme.dart';
 
 class ExpandableConstrainedBox extends StatefulWidget {
   Widget child;
