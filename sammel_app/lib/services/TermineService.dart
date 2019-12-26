4002f869e91a185542d94567dd43ca0525e07379
kybernetik <chemo@mailbox.org>
2019-12-26 01:13:15 +0100

    - DemoTermineService um TerminDetails erweitert - Demo-Modus mit funktionierendem Filter ausgestattet - neue funktionierende Demo-App

--- sammel_app/lib/services/TermineService.dart
+++ sammel_app/lib/services/TermineService.dart
@@ -5,9 +5,11 @@ import 'dart:math';
 import 'package:http_server/http_server.dart';
 import 'package:sammel_app/model/Ort.dart';
 import 'package:sammel_app/model/Termin.dart';
+import 'package:sammel_app/model/TerminDetails.dart';
 import 'package:sammel_app/model/TermineFilter.dart';
 import 'package:sammel_app/services/RestFehler.dart';
 import 'package:sammel_app/services/Service.dart';
+import 'package:sammel_app/shared/ChronoHelfer.dart';
 
 abstract class AbstractTermineService extends Service {
   Future<List<Termin>> ladeTermine(TermineFilter filter);
@@ -20,7 +22,7 @@ abstract class AbstractTermineService extends Service {
 class TermineService extends AbstractTermineService {
   Future<List<Termin>> ladeTermine(TermineFilter filter) async {
     HttpClientResponseBody response =
-    await post(Uri.parse('/service/termine'), jsonEncode(filter));
+        await post(Uri.parse('/service/termine'), jsonEncode(filter));
     if (response.response.statusCode == 200) {
       final termine = (response.body as List)
           .map((jsonTermin) => Termin.fromJson(jsonTermin))
@@ -37,7 +39,7 @@ class TermineService extends AbstractTermineService {
 
   Future<Termin> createTermin(Termin termin) async {
     var response =
-    await post(Uri.parse('service/termine/neu'), jsonEncode(termin));
+        await post(Uri.parse('service/termine/neu'), jsonEncode(termin));
     if (response.response.statusCode == 200) {
       return Termin.fromJson(response.body);
     } else {
@@ -61,57 +63,72 @@ class TermineService extends AbstractTermineService {
 
 class DemoTermineService extends AbstractTermineService {
   static Ort nordkiez =
-  Ort(1, 'Friedrichshain-Kreuzberg', 'Friedrichshain Nordkiez');
+      Ort(1, 'Friedrichshain-Kreuzberg', 'Friedrichshain Nordkiez');
   static Ort treptowerPark = Ort(2, 'Treptow-Köpenick', 'Treptower Park');
   static Ort goerli =
-  Ort(3, 'Friedrichshain-Kreuzberg', 'Görlitzer Park und Umgebung');
+      Ort(3, 'Friedrichshain-Kreuzberg', 'Görlitzer Park und Umgebung');
   static var heute = DateTime.now();
   List<Termin> termine = [
     Termin(
         1,
-        DateTime(heute.year - 1, heute.month - 1, heute.day - 1, 9, 0, 0),
-        DateTime(heute.year - 1, heute.month - 1, heute.day - 1, 12, 0, 0),
+        DateTime(heute.year, heute.month - 1, heute.day - 1, 9, 0, 0),
+        DateTime(heute.year, heute.month - 1, heute.day - 1, 12, 0, 0),
         nordkiez,
         'Sammel-Termin',
-        null /*TerminDetails('Weltzeituhr', 'Bringe Westen und Klämmbretter mit',
-            'Ruft mich an unter 01234567')*/
-    ),
+        TerminDetails('Weltzeituhr', 'Bringe Westen und Klämmbretter mit',
+            'Ruft mich an unter 01234567')),
     Termin(
         2,
-        DateTime(heute.year, heute.month, heute.day, 11, 0, 0),
-        DateTime(heute.year, heute.month, heute.day, 13, 0, 0),
+        DateTime(heute.year, heute.month, heute.day - 1, 11, 0, 0),
+        DateTime(heute.year, heute.month, heute.day - 1, 13, 0, 0),
         treptowerPark,
         'Sammel-Termin',
-        null /*TerminDetails('Hinter der 3. Parkbank links',
-            'wir machen die Parkeingänge', 'Schreibt mir unter e@mail.de')*/
-    ),
+        TerminDetails('Hinter der 3. Parkbank links',
+            'wir machen die Parkeingänge', 'Schreibt mir unter e@mail.de')),
     Termin(
         3,
         DateTime(heute.year, heute.month, heute.day, 23, 0, 0),
         DateTime(heute.year, heute.month, heute.day + 1, 2, 0, 0),
         goerli,
         'Sammel-Termin',
-        null /*TerminDetails('wir telefonieren uns zusammen', 'bitte seid pünktlich',
-            'Meine Handynummer ist 01234567')*/
-    ),
+        TerminDetails('wir telefonieren uns zusammen', 'bitte seid pünktlich',
+            'Meine Handynummer ist 01234567')),
     Termin(
         4,
         DateTime(heute.year, heute.month, heute.day + 1, 18, 0, 0),
         DateTime(heute.year, heute.month, heute.day + 1, 20, 30, 0),
         treptowerPark,
         'Info-Veranstaltung',
-        null /*TerminDetails(
+        TerminDetails(
             'DGB-Haus, Raum 1312',
             'Ihr seid alle herzlich eingeladen zur Strategiediskussion',
-            'Meldet euch doch bitte an unter info@dwenteignen.de damit wir das Buffet planen können')*/
-    ),
+            'Meldet euch doch bitte an unter info@dwenteignen.de damit wir das Buffet planen können')),
   ];
 
   @override
   Future<List<Termin>> ladeTermine(TermineFilter filter) async {
-    // Wert muss als Future herausgereicht werden
-    //return (new Completer()..complete(termine)).future;
-    return termine;
+    return termine.where((termin) {
+      var von = DateTime(termin.beginn?.year, termin.beginn.month,
+          termin.beginn.day, filter.von?.hour ?? 0, filter.von?.minute ?? 0);
+      var bis = DateTime(termin.beginn.year, termin.beginn.month,
+          termin.beginn.day, filter.bis?.hour ?? 0, filter.bis?.minute ?? 0);
+      DateTime datum =
+          DateTime(termin.beginn.year, termin.beginn.month, termin.beginn.day);
+      return (filter.von == null ? true : termin.ende.isAfter(von)) &&
+          (filter.bis == null ? true : termin.beginn.isBefore(bis)) &&
+          (filter.tage == null || filter.tage.isEmpty
+              ? true
+              : filter.tage.contains(datum)) &&
+          (filter.tage == null || filter.tage.isEmpty
+              ? true
+              : filter.tage.contains(datum)) &&
+          (filter.orte == null || filter.orte.isEmpty
+              ? true
+              : filter.orte.map((o) => o.ort).contains(termin.ort.ort)) &&
+          (filter.typen == null || filter.typen.isEmpty
+              ? true
+              : filter.typen.contains(termin.typ));
+    }).toList();
   }
 
   @override
@@ -122,9 +139,8 @@ class DemoTermineService extends AbstractTermineService {
     return await termin;
   }
 
-  // TODO: implement getTermin
   @override
-  Future<Termin> getTerminMitDetails(int id) {
-    return null;
+  Future<Termin> getTerminMitDetails(int id) async {
+    return termine.firstWhere((termin) => termin.id == id);
   }
 }
