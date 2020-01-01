4c8ac1a9d81f25dcb0766dcc5bf6e17501da77d8
kybernetik <chemo@mailbox.org>
2020-01-01 18:45:06 +0100

    androidx-Port

---
+++ sammel_app/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\MBWXW035\Tools\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\MBWXW035\Projekte\dwe\sammel_app"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\MBWXW035\Tools\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=1"
