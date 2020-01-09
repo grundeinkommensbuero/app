09429957eba1febee1038e63d82225aa1f2c40ea
kybernetik <chemo@mailbox.org>
2020-01-09 01:35:15 +0100

    weiterer Versuch CD zu reparieren

--- sammel_app/android/app/src/main/kotlin/de/kybernetik/sammel_app/MainActivity.kt
+++ sammel_app/android/app/src/main/kotlin/de/kybernetik/sammel_app/MainActivity.kt
@@ -1,6 +1,6 @@
 package de.kybernetik.sammel_app
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
