474866663d1766830be86cfcfb138dd04b1b6646
kybernetik <chemo@mailbox.org>
2020-02-21 14:40:54 +0100

    Tests für Farben verschoben

--- sammel_app/test/routes/TerminCard_test.dart
+++ sammel_app/test/routes/TerminCard_test.dart
@@ -1,6 +1,5 @@
 import 'package:sammel_app/routes/TerminCard.dart';
 import 'package:sammel_app/shared/ChronoHelfer.dart';
-import 'package:sammel_app/shared/DweTheme.dart';
 import 'package:test/test.dart';
 
 import '../model/Ort_test.dart';
@@ -57,62 +56,6 @@ void main() {
 
       expect(datumText.substring(0, datumText.indexOf(',')),
           ChronoHelfer.wochentag(spaeter));
-    });
-  });
-  group('faerbeVergangeneTermine', () {
-    test('colors past actions', () {
-      DateTime gestern = DateTime.now().subtract(Duration(days: 1));
-      expect(TerminCard.actionColor(gestern, false),
-          equals(DweTheme.yellowBright));
-
-      DateTime vor1Stunde = DateTime.now().subtract(Duration(hours: 1));
-      expect(TerminCard.actionColor(vor1Stunde, false),
-          equals(DweTheme.yellowBright));
-
-      DateTime vor1Minute = DateTime.now().subtract(Duration(minutes: 1));
-      expect(TerminCard.actionColor(vor1Minute, false),
-          equals(DweTheme.yellowBright));
-    });
-
-    test('colors future actions', () {
-      DateTime morgen = DateTime.now().add(Duration(days: 1));
-      expect(TerminCard.actionColor(morgen, false),
-          equals(DweTheme.yellowLight));
-
-      DateTime in1Stunde = DateTime.now().add(Duration(hours: 1));
-      expect(TerminCard.actionColor(in1Stunde, false),
-          equals(DweTheme.yellowLight));
-
-      DateTime in1Minute = DateTime.now().add(Duration(minutes: 1));
-      expect(TerminCard.actionColor(in1Minute, false),
-          equals(DweTheme.yellowLight));
-    });
-    test('colors own past actions', () {
-      DateTime gestern = DateTime.now().subtract(Duration(days: 1));
-      expect(TerminCard.actionColor(gestern, true),
-          equals(DweTheme.greenLight));
-
-      DateTime vor1Stunde = DateTime.now().subtract(Duration(hours: 1));
-      expect(TerminCard.actionColor(vor1Stunde, true),
-          equals(DweTheme.greenLight));
-
-      DateTime vor1Minute = DateTime.now().subtract(Duration(minutes: 1));
-      expect(TerminCard.actionColor(vor1Minute, true),
-          equals(DweTheme.greenLight));
-    });
-
-    test('colors own future actions', () {
-      DateTime morgen = DateTime.now().add(Duration(days: 1));
-      expect(TerminCard.actionColor(morgen, true),
-          equals(DweTheme.green));
-
-      DateTime in1Stunde = DateTime.now().add(Duration(hours: 1));
-      expect(TerminCard.actionColor(in1Stunde, true),
-          equals(DweTheme.green));
-
-      DateTime in1Minute = DateTime.now().add(Duration(minutes: 1));
-      expect(TerminCard.actionColor(in1Minute, true),
-          equals(DweTheme.green));
     });
   });
   group('erzeugeOrtText', () {
