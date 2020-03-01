800535e996ad18b2a792e7fa24685361fc68515b
kybernetik <chemo@mailbox.org>
2020-03-01 06:06:29 +0100

    Erweiterung der Orte um Geokoordinaten (Closes #54) - Datenbank

--- sammel_db/Skripte/erzeugeStammOrte.sql
+++ sammel_db/Skripte/erzeugeStammOrte.sql
@@ -1,8 +1,10 @@
 create table Stamm_Orte
 (
-    id     int auto_increment not null,
-    bezirk varchar(40) not null,
-    ort    varchar(40) not null,
+    id          int auto_increment not null,
+    bezirk      varchar(40) not null,
+    ort         varchar(40) not null,
+    lattitude   double   not null,
+    longitude   double   not null,
     constraint Stamm_Orte_pk
         primary key (id)
 )
