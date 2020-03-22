3d980de5ad74c317dfa82ea9d8756f8964351ba6
kybernetik <chemo@mailbox.org>
2020-03-22 17:49:52 +0100

    Sammel-Orte von Webseiten-Karte anzeigen #53 - Info-Dialog

--- sammel_app/ios/Runner/GeneratedPluginRegistrant.m
+++ sammel_app/ios/Runner/GeneratedPluginRegistrant.m
@@ -28,6 +28,12 @@
 @import sqflite;
 #endif
 
+#if __has_include(<url_launcher/FLTURLLauncherPlugin.h>)
+#import <url_launcher/FLTURLLauncherPlugin.h>
+#else
+@import url_launcher;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
@@ -35,6 +41,7 @@
   [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
   [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
   [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
+  [FLTURLLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTURLLauncherPlugin"]];
 }
 
 @end
--- sammel_app/test/TestdataStorage.dart
+++ sammel_app/test/TestdataStorage.dart
@@ -1,12 +1,12 @@
 import 'package:sammel_app/model/ListLocation.dart';
 
-curry36() =>
+ListLocation curry36() =>
     ListLocation(1, "Curry 36", "Mehringdamm", "36", 52.4935584, 13.3877282);
 
-cafeKotti() => ListLocation(
+ListLocation cafeKotti() => ListLocation(
     1, "Café Kotti", "Adalbertstraße", "96", 52.5001477, 13.4181523);
 
-zukunft() =>
+ListLocation zukunft() =>
     ListLocation(1, "Zukunft", "Laskerstraße", "5", 52.5016524, 13.4655402);
 
-sampleListLocations() => [curry36(), cafeKotti(), zukunft()];
+List<ListLocation> sampleListLocations() => [curry36(), cafeKotti(), zukunft()];
