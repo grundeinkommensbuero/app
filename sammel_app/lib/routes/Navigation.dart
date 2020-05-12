b952069b510aae181a4b4579b3632e97d73682cf
kybernetik <chemo@mailbox.org>
2020-05-12 22:29:55 +0200

    Animation auch für pageBack und backtoActionPage

--- sammel_app/lib/routes/Navigation.dart
+++ sammel_app/lib/routes/Navigation.dart
@@ -173,14 +173,14 @@ class NavigationState extends State<Navigation>
     if (history.isEmpty)
       closeApp = true;
     else {
-      setState(() => navigation = history.last);
+      switchPage(history.last);
       history.removeLast();
     }
     return closeApp;
   }
 
   void navigateToActionPage() {
-    setState(() => navigation = 0);
+    switchPage(0);
     history.removeLast();
   }
 }
