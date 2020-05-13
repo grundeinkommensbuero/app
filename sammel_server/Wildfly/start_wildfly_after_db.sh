d76ece405eb405a1e4eee76c12a60037427d829e
kybernetik <chemo@mailbox.org>
2020-05-14 01:15:45 +0200

    Test-Server ausrollen #31 - Server- und Datenbank-Credentials aus der Wildfly-Konfiguration rausziehen

--- sammel_server/Wildfly/start_wildfly_after_db.sh
+++ sammel_server/Wildfly/start_wildfly_after_db.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-printf "%s" "waiting for database to run ..."
+db_url=$1
+db_port=$2
+db_schema=$3
+db_user=$4
+db_password=$5
+key_password=$6
+printf "%s" "waiting for database $1:$2/$3 to run ..."
 i=1
-while ! (echo >/dev/tcp/db/3306) &> /dev/null
+while ! (echo >/dev/tcp/$db_url/$db_port) &> /dev/null
 do
   i=$((i+1))
   sleep 1
@@ -13,4 +19,12 @@ do
   fi
 done
 printf "\n%s\n"  "Database responded, starting server..."
-/opt/jboss/wildfly/bin/standalone.sh -b "0.0.0.0" -bmanagement "0.0.0.0"
+/opt/jboss/wildfly/bin/standalone.sh \
+  -b "0.0.0.0" \
+  -bmanagement "0.0.0.0" \
+  -DDB_URL=$db_url \
+  -DDB_PORT=$db_port \
+  -DDB_SCHEMA=$db_schema \
+  -DDB_USER=$db_user \
+  -DDB_PASSWORD=$db_password \
+  -DKEY_PASSWORD=$key_password
