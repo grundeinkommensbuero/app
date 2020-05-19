c34d375030e3a723d676ba4135358363081b3eff
kybernetik <dwesammelapp@googlemail.com>
2020-05-19 18:50:38 +0200

    ios deployment

--- sammel_app/ios/Flutter/flutter_export_environment.sh
+++ sammel_app/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\MBWXW035\Tools\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\MBWXW035\Projekte\dwe\sammel_app"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/anna/DWE/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/anna/DWE/sammel-app/sammel_app"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT***REMOVED***/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\Users\MBWXW035\Tools\flutter\bin\cache\artifacts\engine\ios"
-export "FLUTTER_BUILD_NAME=0.1.0"
-export "FLUTTER_BUILD_NUMBER=3"
+export "SYMROOT=${SOURCE_ROOT***REMOVED***/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/anna/DWE/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_BUILD_NAME=0.2.0"
+export "FLUTTER_BUILD_NUMBER=8"
