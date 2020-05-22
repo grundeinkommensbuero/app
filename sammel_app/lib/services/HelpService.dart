a517e1ff67a08e0183670660c736e374315f2f85
kybernetik <chemo@mailbox.org>
2020-05-22 20:13:34 +0200

    FAQ-Seite #68 - bessere Beispiel-Texte

--- sammel_app/lib/services/HelpService.dart
+++ sammel_app/lib/services/HelpService.dart
@@ -3,19 +3,26 @@ import 'package:sammel_app/model/Help.dart';
 
 class HelpService {
   static List<Help> helps = [
-    Help(1, 'Erster Eintrag',
-        Text('Dies ist der erste Eintrag und er ist sehr gut'),
-        Text('Dies ist der erste Eintrag und er ist...'), ['Tag', 'Tag2']),
-    Help(
-        2,
-        'Zweiter Eintrag',
-        Text('Dies ist der zweite Eintrag und er ist noch besser'),
-        Text('Dies ist der zweite Eintrag und er ist...'),
+    Help(1,
+        'Was kostet Enteignung? Unsere Modellrechnungen zur Entschädigungshöhe',
+        Text('Wie viel Entschädigung muss den Wohnungskonzernen deren Wohnungen vergesellschaftet werden gezahlt werden? Diese Frage treibt viele um. Bereits in unserem Kurzgutachten haben wir festegstellt: Das ist eine politische Entscheidung! Sogar eine nominelle Entschädigung ist möglich.\n\n'
+            'Wir haben verschiedene Modelle durchgerechnet. Von der nominellen Entschädigung bis zum Zweckentschädigungsmodell. Letzteres beruht darauf, dass nicht mehr Entschädigung gezahlt werden darf, als notwendig ist. Voraussetzung ist, dass der Vergesellschaftungszweck erfüllt wird. Die Überführung der Wohnungen in die Gemeinwirtschaft, so dass sie für alle leistbar sind. Der Zweck der Vergesellschaftung setzt der Entschädigungshöhe enge Grenzen.\n\n'
+            'Mit verschiedenen Modellen kommen wir so auf eine Summe von 1,5 bis 2,4 Mrd, die vom Landeshaushalt direkt getragen werden müssten.\n\n'
+            'Der Rest ist, wie allgemein üblich beim Erwerb von Immobilien, kreditfinanziert und wird über bezahlbare Mieten abbezahlt. Die gesamte Entschädigungssumme beläuft sich für 200.000 Wohnungen somit auf 7,3 bis 12 Mrd. Euro, exklusive der Möglichkeit der nominellen Entschädigung.'),
+        Text('Wie viel Entschädigung muss den Wohnungskonzernen deren Wohnungen vergesellschaftet werden gezahlt werden? Diese Frage treibt viele um. Bereits in unserem Kurzgutachten haben wir festegstellt...'),
         ['Tag']),
-    Help(3, 'Dritter Eintrag    ',
+    Help(2, 'Enteignung kann haushaltsneutral sein',
+        Text('Der Berliner Senat stellte im März 2019 eine Kostenschätzung zum Volksbegehren „Deutsche Wohnen & Co enteignen“ vor.  Seit dem 4.3. zirkulierten in der Presse Mutmaßungen über Ent­schädigungskosten zwischen 28,8 und 36 Milliarden Euro – Zahlen, die wir zunächst nicht über­prüfen konnten, denn erst am 7.3. lag uns die aus­führliche Begründung vor. Doch am 26.3. kün­digte der Senat dann an, seine Kostenschät­zung korrigieren zu wollen. Tatsächlich liegt erst am heutigen Tag eine erneute Kosten­schätzung vor, die substantiell nichts ändert und zu der wir nun Stellung nehmen.\n\n'
+  'Eine genaue Lektüre der amtlichen Schätzung ergibt: Der Senat stimmt uns zu, dass eine Ent­schädigung deutlich unter Verkehrswert möglich ist, und schlägt selbst eine Unter­schreitung des Verkehrswerts vor, indem er Wertsteige­rungen der Immobilien herausrechnen will, die nicht auf Eigenleistungen der Unternehmen be­ruhen. Dieser Ansatz ist allerdings nach einem Rechtsgutachten, das die Senatsverwaltung für Stadt­entwicklung und Wohnen beim Verwaltungs­rechtler Dr. Geulen in Auftrag gab, nur bei Ent­schädigungen für kleinere, selbst ge­nutzte Immo­bilien an­gemessen, nicht für das Portfolio von Immobilienunternehmen – die Entschädigung von Eigentum, das auf den Finanzmärkten erworben sei, ermögliche und gebiete eine stärkere Redu­zierung gegenüber dem Ver­kehrswert. Doch selbst seinen zaghaften Ansatz handhabt der Senat inkonsequent\n\n'
+  'Eine Herausrechnung der nicht auf Eigenleistungen der Unternehmen beruhenden Wertsteige­rungen führt zu einer Ent­schädigung von nur 18,1 Milliar­den Euro, nicht 28,8 Milliarden Euro. Zudem enthält die Be­rechnung der laufenden Folgekosten der von uns vorgeschlagenen Anstalt öffentlichen Rechts (AöR) eine wesentliche Unklarheit: Es scheint, als würde bei den laufenden Kapitalkosten so­wohl von gleichbleibenden Tilgungsraten als auch von gleichbleibenden Zins­kosten ausgegan­gen – dies wäre ein methodischer Fehler, denn Tilgung senkt die Zinskosten; wird wohlwollend ein Annuitätendarlehen mit steigenden Tilgungsraten unterstellt, macht der Senat den Tilgungs­zeitraum nicht transparent, der sich nur indirekt auf 45 Jahre errechnen lässt. Auf unsere Bitte um Klärung reagierte der Senat nicht. Klar ist jedoch: Bei einem Tilgungszeitraum von 45 Jahren können die gesamten Kapitalkos­ten einer Entschädigung in Höhe von 18,1 Milliarden Euro zzgl. Erwerbsnebenkosten voll­ständig aus den Mieteinnahmen der AöR getragen werden. Die Mieten lassen sich sogar so­fort um monatlich 0,97 € pro m² senken. Für das Land Berlin wäre die Vergesellschaftung haushaltsneutral, wäh­rend für eine halbe Million Mieter*innen die Mieten sinken. Dies, wohlgemerkt, mit den Metho­den und Zahlen des Senats gerechnet.\n\n'
+  'Klar ist jedoch: Bei einem Tilgungszeitraum von 45 Jahren können die gesamten Kapitalkos­ten einer Entschädigung in Höhe von 18,1 Milliarden Euro zzgl. Erwerbsnebenkosten voll­ständig aus den Mieteinnahmen der AöR getragen werden. Die Mieten lassen sich sogar so­fort um monatlich 0,97 € pro m² senken. Für das Land Berlin wäre die Vergesellschaftung haushaltsneutral, wäh­rend für eine halbe Million Mieter*innen die Mieten sinken. Dies, wohlgemerkt, mit den Metho­den und Zahlen des Senats gerechnet.\n\n'
+  'Legt man das von uns vorge­schlagene Faire-Mieten-Mo­dell („Zweckentschädigungsverfahren“) zugrunde, das die Interes­sen der Allgemeinheit an einer Vergesellschaftung angemessen berücksichtigt und sozialverträg­liche Mieten in den Mittelpunkt der Berechnung stellt, so können die Belastungen noch geringer ausfallen. Vergesellschaftung ist daher nicht teuer: Die AöR kann die gesamten Vergesellschaf­tungskosten allein aus den Mieten stemmen, die sogar gesenkt werden können. Nach Tilgung ste­hen erhebliche Mittel für weitere Mietsenkungen und für die Finanzierung von Neubau zur Ver­fügung.'),
+        Text('Der Berliner Senat stellte im März 2019 eine Kostenschätzung zum Volksbegehren „Deutsche Wohnen & Co enteignen“ vor.  Seit dem 4.3. zirkulierten in der Presse Mutmaßungen über Entschädigungskosten...'), ['Tag', 'Tag2']),
+    Help(3, 'Corona-Krise: Sicherheit für Mieter*innen, statt Profite für Immobilienkonzerne!',
         Column(children: [
-          Text('Dies ist der dritte Eintrag und er ist der beste. Außerdem hat er ein Bild:'),
+          Text('In der Corona-Krise verlieren viele Menschen ihren Job und können ihre Miete nicht zahlen, Immobilienkonzerne verdienen einfach weiter. “Zuhausebleiben” geht nur, wenn man ein Zuhause hat, weswegen wir Forderungen formuliert haben, was jetzt in der Corona-Krise zu tun ist, um Wohnraum für alle sicherzustellen.\n'
+          '„Ob Menschen auch in Zeiten der Corona-Pandemie ein sicheres Zuhause haben, darf nicht von der Güte des Vermieters abhängen. Wir brauchen schnelle gesetzliche Regelungen und Sicherheit für Mieter*innen. Mieter*innen dürfen nach der Krise nicht vor einem Schuldenberg stehen“ so Susanna Raab von unserer Initiative.'),
           Image.asset('assets/images/dwe.png')]),
-        Text('Dies ist der dritte Eintrag und er ist...'), ['Tag2'])
+        Text('In der Corona-Krise verlieren viele Menschen ihren Job und können ihre Miete nicht zahlen, Immobilienkonzerne verdienen einfach weiter. “Zuhausebleiben” geht nur, wenn man ein Zuhause ...'), ['Tag2'])
   ];
 ***REMOVED***
