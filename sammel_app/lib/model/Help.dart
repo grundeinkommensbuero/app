aa8c1f18732bae6354a2856fd936183abae223b8
kybernetik <chemo@mailbox.org>
2020-05-22 16:27:59 +0200

    FAQ-Seite #68 - Dynamische Funktionen wieder ausgebaut

--- sammel_app/lib/model/Help.dart
+++ sammel_app/lib/model/Help.dart
@@ -1,33 +1,11 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
 
 class Help {
   int id;
   String title;
-  String content;
-  var shortContent;
+  Widget content;
+  Widget shortContent;
   List<String> tags;
 
   Help(this.id, this.title, this.content, this.shortContent, this.tags);
-
-  Help.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        title = json['title'] ?? '',
-        content = json['content'] ?? '',
-        shortContent = json['shortContent'] ?? '',
-        tags =  List<String>.from(json['tags']);
-
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'title': title,
-        'content': content,
-        'shortContent': shortContent,
-        'tags': tags,
-      };
-
-  bool equals(Help that) =>
-      this.id == that.id &&
-      this.title == that.title &&
-      this.content == that.content &&
-      this.shortContent == that.shortContent &&
-      listEquals(this.tags, that.tags);
 }
