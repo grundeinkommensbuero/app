93663037d2a65697592783e9b98c4317b7db2c61
kybernetik <chemo@mailbox.org>
2020-05-22 14:54:09 +0200

    FAQ-Seite #68 - Navigation-Erweiterung um FAQ-Seite

---
+++ sammel_app/lib/routes/FAQ.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sammel_app/shared/DweTheme.dart';
+
+class FAQ extends StatefulWidget {
+  FAQ() : super(key: Key('faq page'));
+
+  @override
+  State<StatefulWidget> createState() => FAQState();
+***REMOVED***
+
+class FAQState extends State<FAQ> {
+  var helps = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView(children: helps.map((help) => HelpCard(help)).toList()));
+  ***REMOVED***
+***REMOVED***
+
+class HelpCard extends StatelessWidget {
+  final help;
+
+  HelpCard(this.help) : super(key: Key('help tile'));
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(color: DweTheme.yellowLight, boxShadow: [
+        BoxShadow(
+            color: Colors.grey,
+            offset: Offset(1.0, 1.0),
+            blurRadius: 3.0,
+            spreadRadius: 1.0)
+      ]),
+      child: Column(
+        children: [Text('Title')],
+      ),
+    );
+  ***REMOVED***
+***REMOVED***
