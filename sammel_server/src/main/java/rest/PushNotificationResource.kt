09973cc701dfd93a8cbd532ca5c8adcecba772c6
kybernetik <chemo@mailbox.org>
2020-05-26 00:31:08 +0200

    Push-Notification-Schnittstelle zu Firebase #88 - Implementation ohne Tests

---
+++ sammel_server/src/main/java/rest/PushNotificationResource.kt
@@ -0,0 +1,37 @@
+package rest
+
+import services.FirebaseService
+import java.lang.Exception
+import javax.ejb.EJB
+import javax.ws.rs.Consumes
+import javax.ws.rs.POST
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+
+@Path("push")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+open class PushNotificationResource {
+
+    @EJB
+    private lateinit var firebase: FirebaseService
+
+    @Path("devices")
+    @POST
+    open fun pushToDevices(nachricht: PushMessageDto) {
+        if (nachricht.recipients.isNullOrEmpty())
+            throw MissingMessageTarget("Die Nachricht enthält keine Empfänger")
+        firebase.sendePushNachrichtAnEmpfaenger(nachricht.notification, nachricht.data, nachricht.recipients!!)
+    ***REMOVED***
+
+    @Path("topic")
+    @POST
+    open fun pushToTopic(nachricht: PushMessageDto) {
+        if (nachricht.recipients.isNullOrEmpty())
+            throw MissingMessageTarget("Die Nachricht enthält kein Topic")
+        firebase.sendePushNachrichtAnTopic(nachricht.notification, nachricht.data, nachricht.topic!!)
+    ***REMOVED***
+
+    class MissingMessageTarget(message: String) : Exception(message)
+***REMOVED***
