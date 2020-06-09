99028f60f96b985216b0cdc3ec7ed21eecc7680b
kybernetik <chemo@mailbox.org>
2020-06-09 20:40:24 +0200

    Push-Schnittstelle #89

---
+++ sammel_server/src/main/java/services/Firebase.kt
@@ -0,0 +1,33 @@
+package services
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
+import org.jboss.logging.Logger
+import java.io.FileInputStream
+import javax.annotation.PostConstruct
+import javax.ejb.Singleton
+import javax.ejb.Startup
+
+
+@Startup
+@Singleton
+open class Firebase() {
+    private val LOG = Logger.getLogger(Firebase::class.java)
+
+    @PostConstruct
+    @Suppress("unused")
+    open fun initializeFirebase() {
+        val creds = FileInputStream("${System.getenv("JBOSS_HOME")***REMOVED***/standalone/configuration/sammel-app-firebase-adminsdk.json")
+
+        FirebaseApp.initializeApp(FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(creds))
+                .setDatabaseUrl("https://sammel-app.firebaseio.com")
+                .build())
+        LOG.info("Firebase wurde intialisiert für ${FirebaseApp.DEFAULT_APP_NAME***REMOVED***")
+    ***REMOVED***
+
+    open fun instance(): FirebaseMessaging = FirebaseMessaging.getInstance()
+
+***REMOVED***
--- sammel_server/src/main/java/services/FirebaseService.kt
+++ sammel_server/src/main/java/services/FirebaseService.kt
@@ -1,16 +1,12 @@
 package services
 
-import com.google.auth.oauth2.GoogleCredentials
-import com.google.firebase.FirebaseApp
-import com.google.firebase.FirebaseOptions
-import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.Message
 import com.google.firebase.messaging.MulticastMessage
 import com.google.firebase.messaging.Notification
 import org.jboss.logging.Logger
 import rest.PushNotificationDto
-import java.io.FileInputStream
-import javax.annotation.PostConstruct
+import java.lang.Exception
+import javax.ejb.EJB
 import javax.ejb.Singleton
 import javax.ejb.Startup
 
@@ -19,20 +15,17 @@ import javax.ejb.Startup
 open class FirebaseService {
     private val LOG = Logger.getLogger(FirebaseService::class.java)
 
-
-    @PostConstruct
-    @Suppress("unused")
-    fun initializeFirebase() {
-        val creds = FileInputStream("${System.getenv("JBOSS_HOME")***REMOVED***/standalone/configuration/sammel-app-firebase-adminsdk.json")
-
-        FirebaseApp.initializeApp(FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(creds))
-                .setDatabaseUrl("https://sammel-app.firebaseio.com")
-                .build())
-    ***REMOVED***
+    @EJB
+    private lateinit var firebase: Firebase
 
     open fun sendePushNachrichtAnEmpfaenger(notification: PushNotificationDto?, data: Map<String, String>?, empfaenger: List<String>) {
         LOG.debug("Sende Nachricht $notification/$data an Empfänger $empfaenger")
+
+        if (empfaenger.isEmpty())
+            throw MissingMessageTarget("Die Nachricht enthält keine Empfänger")
+
+        if (empfaenger.size > 500) throw TooManyRecipientsError()
+
         // Quelle: https://firebase.google.com/docs/cloud-messaging/send-message#send-messages-to-multiple-devices
         val message = MulticastMessage.builder()
                 .setNotification(
@@ -41,12 +34,16 @@ open class FirebaseService {
                 .putAllData(data)
                 .addAllTokens(empfaenger)
                 .build()
-        val response = FirebaseMessaging.getInstance().sendMulticast(message)
+        val response = firebase.instance().sendMulticast(message)
         LOG.debug("${response.successCount***REMOVED***  Nachrichten wurden erfolgreich versendet")
     ***REMOVED***
 
     open fun sendePushNachrichtAnTopic(notification: PushNotificationDto?, data: Map<String, String>?, topic: String) {
         LOG.debug("Sende Nachricht $notification/$data an Thema $topic")
+
+        if (topic.isEmpty())
+            throw MissingMessageTarget("Die Nachricht enthält kein Topic")
+
         // Quelle: https://firebase.google.com/docs/cloud-messaging/send-message#send-messages-to-topics
         val message: Message = Message.builder()
                 .setNotification(
@@ -56,10 +53,13 @@ open class FirebaseService {
                 .setTopic(topic)
                 .build()
 
-        val response = FirebaseMessaging.getInstance().send(message)
+        val response = firebase.instance().send(message)
         LOG.debug("Erfolgreich Nachricht an Topic $topic gesendet: $response")
     ***REMOVED***
 
     open fun sendeNachrichtAnAlle(notification: PushNotificationDto?, data: Map<String, String>?) =
             sendePushNachrichtAnTopic(notification, data, "global")
+
+    class MissingMessageTarget(message: String) : Exception(message)
+    class TooManyRecipientsError: Exception("Nicht mehr als 500 Empfänger für eine Nachricht möglich")
 ***REMOVED***
