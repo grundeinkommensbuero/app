774c6563c20cc149d14840b2a58f0841d0b6f03a
kybernetik <chemo@mailbox.org>
2020-06-13 18:12:21 +0200

    Benutzer im Backend einführen (Closes #84) - Fixes

--- sammel_db/Skripte/fuelleBenutzerTabelle.sql
+++ sammel_db/Skripte/fuelleBenutzerTabelle.sql
@@ -1,2 +1,5 @@
-INSERT INTO Benutzer (id, name, passwort, telefonnummer) VALUES (1, 'Karl Marx', 'Expropriation!', '123456789');
-INSERT INTO Benutzer (id, name, passwort, telefonnummer) VALUES (2, 'Rosa Luxemburg', 'Ich bin, ich war, ich werde sein', null);
+INSERT INTO Benutzer (id, name) VALUES (1, 'Karl Marx');
+INSERT INTO Benutzer (id, name) VALUES (2, 'Rosa Luxemburg');
+
+INSERT INTO Credentials (id, secret, salt, firebasekey) VALUES (1, 'hash11111111', 'salt11111111', 'AAAAAAAA');
+INSERT INTO Credentials (id, secret, salt, firebasekey) VALUES (2, 'hash22222222', 'salt22222222', 'BBBBBBBB');
