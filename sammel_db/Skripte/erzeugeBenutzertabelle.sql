c9918141cb04e3a404eaf231f151f457c40b0d9f
kybernetik <chemo@mailbox.org>
2020-06-13 18:12:21 +0200

    Benutzer im Backend einführen (Closes #84) - Fixes

--- sammel_db/Skripte/erzeugeBenutzertabelle.sql
+++ sammel_db/Skripte/erzeugeBenutzertabelle.sql
@@ -1,10 +1,18 @@
 create table Benutzer
 (
     id            int auto_increment not null,
-    name          varchar(40) unique not null,
-    passwort      varchar(100)       not null,
-    telefonnummer varchar(40)        null,
+    name          varchar(40) unique null,
     constraint Benutzer_pk
         primary key (id)
 );
+
+create table Credentials
+(
+    id            int unique not null,
+    secret        varchar(1024) not null,
+    salt          varchar(1024) not null,
+    firebasekey   varchar(256) not null,
+    constraint Credentials_pk
+        primary key (id)
+)
 
