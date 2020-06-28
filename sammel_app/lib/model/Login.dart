14b931ad16c3e222052dee7d46985bbc948ec312
kybernetik <chemo@mailbox.org>
2020-06-28 13:40:34 +0200

    Benutzer anlegen und einloggen im Frontend #91 - User an Backend angepasst

---
+++ sammel_app/lib/model/Login.dart
@@ -0,0 +1,15 @@
+import 'User.dart';
+
+class Login {
+  User user;
+  String secret;
+  String firebaseKey;
+
+  Login(this.user, this.secret, this.firebaseKey);
+
+  Map<String, dynamic> toJson() => {
+        'user': user,
+        'secret': secret,
+        'firebaseKey': firebaseKey,
+      };
+}
---
+++ sammel_app/test/model/User_test.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sammel_app/model/User.dart';
+
+void main() {
+  group('deserialisises', () {
+    test('empty User', () {
+      expect(
+          jsonEncode(User(null, null, null).toJson()),
+          '{'
+          '"id":null,'
+          '"name":null,'
+          '"color":null'
+          '}');
+
+      expect(
+          jsonEncode(User(0, '', null).toJson()),
+          '{'
+          '"id":0,'
+          '"name":"",'
+          '"color":null'
+          '}');
+    });
+
+    test('filled User', () {
+      expect(
+          jsonEncode(User(1, 'secret', Colors.red).toJson()),
+          '{'
+          '"id":1,'
+          '"name":"secret",'
+          '"color":4294198070'
+          '}');
+    });
+  });
+
+  group('deserialises', () {
+    test('empty user', () {
+      User user = User.fromJSON(jsonDecode('{'
+          '"id":null,'
+          '"name":null,'
+          '"color":null'
+          '}'));
+
+      expect(user.id, isNull);
+      expect(user.name, isNull);
+      expect(user.color, isNull);
+
+      user = User.fromJSON(jsonDecode('{'
+          '"id":0,'
+          '"name":"",'
+          '"color":null'
+          '}'));
+
+      expect(user.id, 0);
+      expect(user.name, isEmpty);
+      expect(user.color, isNull);
+    });
+
+    test('filled user', () {
+      User user = User.fromJSON(jsonDecode('{'
+          '"id":1,'
+          '"name":"Karl Marx",'
+          '"color":4294198070'
+          '}'));
+
+      expect(user.id, 1);
+      expect(user.name, 'Karl Marx');
+      expect(user.color.value, Colors.red.value);
+    });
+  });
+}
