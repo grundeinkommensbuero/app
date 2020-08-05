d2752c0b8a848b3c052b57ff00be26523e22a207
kybernetik <chemo@mailbox.org>
2020-08-05 17:54:08 +0200

    Service-Tests: AuthFehler

--- sammel_app/lib/services/AuthFehler.dart
+++ sammel_app/lib/services/AuthFehler.dart
@@ -1,9 +1,7 @@
 import 'package:sammel_app/services/RestFehler.dart';
 
 class AuthFehler extends RestFehler {
-  String reason;
-
-  AuthFehler(String reason) : super(reason);
+  AuthFehler(String message) : super(message);
 
   AuthFehler.fromJson(Map<String, dynamic> json) : super.fromJson(json);
 }
