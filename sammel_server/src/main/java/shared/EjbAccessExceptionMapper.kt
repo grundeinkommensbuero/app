3595e3a3fd00c31f0f940a2bc4c4afbdd6dd8021
kybernetik <chemo@mailbox.org>
2020-10-04 07:15:10 +0200

    Absicherung der Schnittstellen  #94 - ExceptionMapper für 401 statt 500

---
+++ sammel_server/src/main/java/shared/EjbAccessExceptionMapper.kt
@@ -0,0 +1,15 @@
+package shared
+
+import javax.ejb.EJBException
+import javax.ws.rs.core.Response
+import javax.ws.rs.core.Response.Status.FORBIDDEN
+import javax.ws.rs.ext.ExceptionMapper
+import javax.ws.rs.ext.Provider
+
+@Provider
+class EjbAccessExceptionMapper : ExceptionMapper<EJBException?> {
+    override
+    fun toResponse(exception: EJBException?): Response {
+        return Response.status(FORBIDDEN).entity("Du hast nicht die notwendigen Rechte um diese Funktion auszuführen").build()
+    }
+}
