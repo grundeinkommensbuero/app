f5576534ac9c47a4ea6f4578a2d7cd275ee1b0d3
kybernetik <chemo@mailbox.org>
2020-12-05 23:08:55 +0100

    Fehler beim Erstellen des Docker Image nach Anleitung (Closes #139)

--- sammel_server/src/main/java/de/kybernetik/services/Firebase.kt
+++ sammel_server/src/main/java/de/kybernetik/services/Firebase.kt
@@ -3,9 +3,10 @@ package de.kybernetik.services
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
-import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.*
 import org.jboss.logging.Logger
 import java.io.FileInputStream
+import java.io.IOException
 import javax.annotation.PostConstruct
 import javax.ejb.Singleton
 import javax.ejb.Startup
@@ -13,21 +14,41 @@ import javax.ejb.Startup
 
 @Startup
 @Singleton
-open class Firebase() {
+open class Firebase {
     private val LOG = Logger.getLogger(Firebase::class.java)
+    var initialized = false
 
     @PostConstruct
     @Suppress("unused")
     open fun initializeFirebase() {
-        val creds = FileInputStream("${System.getenv("JBOSS_HOME")}/standalone/configuration/sammel-app-firebase-adminsdk.json")
+        try {
+        val creds =
+            FileInputStream("${System.getenv("JBOSS_HOME")}/standalone/configuration/sammel-app-firebase-adminsdk.json")
 
-        FirebaseApp.initializeApp(FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(creds))
-                .setDatabaseUrl("https://sammel-app.firebaseio.com")
-                .build())
-        LOG.info("Firebase wurde intialisiert für ${FirebaseApp.DEFAULT_APP_NAME}")
+            FirebaseApp.initializeApp(
+                FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(creds))
+                    .setDatabaseUrl("https://sammel-app.firebaseio.com")
+                    .build()
+            )
+            initialized = true
+            LOG.info("Firebase wurde initialisiert für ${FirebaseApp.DEFAULT_APP_NAME}")
+        } catch (e: IOException) {
+            LOG.warn("Firebase konnte nicht initialisiert werden: ${e.message}. Möglicherweise liegen die API-Keys nicht vor")
+        }
     }
 
-    open fun instance(): FirebaseMessaging = FirebaseMessaging.getInstance()
+    open fun send(message: Message): String {
+        if (initialized)
+            return FirebaseMessaging.getInstance().send(message)
+        LOG.warn("Nachricht konnte nicht an Firebase versendet werden, weil Firebase nicht initialisiert wurde")
+        return ""
+    }
 
+    open fun sendMulticast(message: MulticastMessage?): BatchResponse? {
+        if (initialized)
+            return FirebaseMessaging.getInstance().sendMulticast(message)
+        LOG.warn("Nachricht konnte nicht an Firebase versendet werden, weil Firebase nicht initialisiert wurde")
+        return null
+    }
 }
