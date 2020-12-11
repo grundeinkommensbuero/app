779444191c3adca89946a5c359a95d507ca02b94
kybernetik <chemo@mailbox.org>
2020-12-12 00:15:34 +0100

    Tests gefixt, Warning beseitigt

--- sammel_app/test/services/PushSendService_test.dart
+++ sammel_app/test/services/PushSendService_test.dart
@@ -63,7 +63,8 @@ main() {
 
       verify(backendMock.post(
           'service/push/topic/Thema',
-          '{"recipients":null,"data":{"encrypted":"Base64","payload":"eyJ0eXBlIjpudWxsfQ=="***REMOVED***,"notification":{"title":"Titel","body":"Inhalt"***REMOVED******REMOVED***',
+          '{"recipients":null,"data":{"type":null***REMOVED***,"notification":{"title":"Titel","body":"Inhalt"***REMOVED******REMOVED***',
+          any,
           any));
     ***REMOVED***);
   ***REMOVED***);
