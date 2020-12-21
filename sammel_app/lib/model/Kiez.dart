9037e733c28c1f8e13bd35bccc9e44611b3bdfa7
kybernetik <chemo@mailbox.org>
2020-12-21 21:50:31 +0100

    Kieze abschließen #62 - LatLng in Kiez

--- sammel_app/lib/model/Kiez.dart
+++ sammel_app/lib/model/Kiez.dart
@@ -1,33 +1,36 @@
+import 'package:latlong/latlong.dart';
 
 class Kiez {
   String kiez;
   String bezirk;
-  // TODO LatLng center;
-  double latitude;
-  double longitude;
+  LatLng center;
   List<List<double>> polygon;
 
-  Kiez(bezirk, kiez, lattitude, longitude) {
+  Kiez(bezirk, kiez, latitude, longitude) {
     this.kiez = kiez;
     this.bezirk = bezirk;
-    this.latitude = lattitude;
-    this.longitude = longitude;
+    this.center = latitude == null || longitude == null
+        ? null
+        : LatLng(latitude, longitude);
   }
 
   Kiez.fromJson(Map<String, dynamic> json)
       : bezirk = json['bezirk'],
         kiez = json['kiez'],
-        latitude = json['longitude'],
-        longitude = json['latitude'],
-        polygon = (json['polygon'] as List).map((e) => (e as List).map((e) => e as double).toList()).toList();
+        center = json['latitude'] == null || json['longitude'] == null
+            ? null
+            : LatLng(json['latitude'], json['longitude']),
+        polygon = (json['polygon'] as List)
+            .map((e) => (e as List).map((e) => e as double).toList())
+            .toList();
 
   String toJson() => kiez;
 
   bool equals(Kiez that) =>
       this.bezirk == that.bezirk &&
       this.kiez == that.kiez &&
-      this.latitude == that.latitude &&
-      this.longitude == that.longitude &&
+      this.center.latitude == that.center.latitude &&
+      this.center.longitude == that.center.longitude &&
       this.polygon.contains(that.polygon) &&
       that.polygon.contains(this.polygon);
 }
