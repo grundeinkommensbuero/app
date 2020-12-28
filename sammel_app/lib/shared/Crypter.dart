096a7b235b085a679d0cb5e2283b876a4566aa4d
kybernetik <chemo@mailbox.org>
2020-12-28 18:24:13 +0100

    Verschlüsselung der Push-Payload #127 - Umsetzung im Frontend

--- sammel_app/lib/shared/Crypter.dart
+++ sammel_app/lib/shared/Crypter.dart
@@ -1,15 +1,29 @@
 import 'dart:convert';
+import 'package:cryptography/cryptography.dart';
+
+final SecretKey key =
+    SecretKey(base64.decode('vue8NkTYyN1e2OoHGcapLZWiCTC+13Eqk9gXBSq4azc='));
 
 Map<dynamic, dynamic> decrypt(dynamic data) {
   if (data['encrypted'] == 'Base64') {
     if (data['payload'] == null) return {};
     var decodiert = jsonDecode(utf8.decode(base64Decode(data['payload'])));
-    print('Push-Nachricht entschlüsselt: ${decodiert}');
+    print('Codierte Push-Nachricht: ${decodiert}');
     return decodiert;
+  }
+  if (data['encrypted'] == 'AES') {
+    if (data['payload'] == null) return {};
+    final nonce = Nonce(base64.decode(data['nonce']));
+    final cipher = CipherWithAppendedMac(aesCbc, Hmac(sha256));
+    final decrypted = cipher.decryptSync(base64.decode(data['payload']),
+        secretKey: key, nonce: nonce);
+    final decoded = jsonDecode(utf8.decode(decrypted));
+    print('Verschlüsselte Push-Nachricht: ${decoded}');
+    return decoded;
   }
   if (data['encrypted'] == 'Plain') {
     if (data['payload'] == null) return {};
-    print('Push-Nachricht unverschlüsselt: ${data['payload']}');
+    print('Uncodierte Push-Nachricht: ${data['payload']}');
     return data['payload'];
   }
   print('Push-Nachricht nicht entschlüsselt: ${data}');
