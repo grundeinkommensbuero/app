a892094206a1e9b0e656d688c62a3d6fedba4133
kybernetik <chemo@mailbox.org>
2021-01-12 04:10:46 +0100

    Push-To-Topic-Helper

---
+++ sammel_app/tools/pushToTopic.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+Future<void> main() async {
+  final body = {
+    "data": {
+      "type": "SimpleChatMessage",
+      "channel": "global",
+      "timestamp": "2020-01-12 03:32:00.000000",
+      "timestamp": "2020-01-12 03:32:00.000000",
+      "text": "text goes here",
+      "color": "2137636880",
+      "sender_name": "Deutsche Wohnen & Co. Enteignen",
+      "user_id": -1
+    ***REMOVED***,
+    "notification": {"title": "Titel", "body": "Meldung"***REMOVED***
+  ***REMOVED***
+
+  final response = await post(
+      'http://127.0.0.1:18080/service/push/topic/global',
+      headers: {
+        "Authorization":
+            "Basic MzpmZjdhOGI2Yi1lMTVhLTRmZWUtOWY3OS1kY2EwNmZkNmM4ODM=",
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      ***REMOVED***,
+      body: jsonEncode(body));
+
+  print(
+      'Ergebnis: ${response.statusCode***REMOVED*** - ${response.body***REMOVED*** - ${response.reasonPhrase***REMOVED***');
+***REMOVED***
