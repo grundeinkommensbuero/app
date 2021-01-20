0808d5d3ebce0dd1ea314f90ca6d2be675083a31
Simon Huesken <simon.huesken@gmail.com>
2021-01-20 12:58:29 +0100

    undo commit dialog because already covered in RestFehelr dialog

--- sammel_app/lib/shared/showUsernameDialog.dart
+++ sammel_app/lib/shared/showUsernameDialog.dart
@@ -64,25 +64,8 @@ class UsernameDialogState extends State<UsernameDialog> {
       await Provider.of<AbstractUserService>(context).updateUsername(username);
       Navigator.pop(context, username);
     } catch (e) {
-      showUsernameUnavailableDialog(context, username)
-      Navigator.pop(context, null);
+      Navigator.pop(context, null)
     }
-  }
-
-  showUsernameUnavailableDialog(context, username) {
-    showDialog(
-        context: context,
-        child: AlertDialog(
-          title: Text('Zu viele Tage'.tr()),
-          content: SelectableText(
-              'Name {username} ist bereits vergeben.'.tr(namedArgs: {'username': username})),
-          actions: <Widget>[
-            RaisedButton(
-              child: Text('Schließen').tr(),
-              onPressed: () => Navigator.pop(context),
-            )
-          ],
-        ));
   }
 
   isValid() => isNotBlank(username);
