582955a21047b7b4fec15d389e1c1e1e4c185142
kybernetik <chemo@mailbox.org>
2021-01-22 02:44:56 +0100

    Korrekturen am News-Tool

--- sammel_app/tools/pushToTopic.dart
+++ sammel_app/tools/pushToTopic.dart
@@ -6,19 +6,19 @@ Future<void> main() async {
   final body = {
     "data": {
       "type": "TopicChatMessage",
-      "channel": "global",
-      "timestamp": "2020-01-12 03:32:00.000000",
-      "timestamp": "2020-01-12 03:32:00.000000",
-      "text": "text fgsdg dfs gdsdddsaasasssa55555fgdsfg dfg dsfgsd fg dsfgdsfgdfsggoes here7",
-      "color": "2137636880",
-      "sender_name": "Deutsche Wohnen & Co. Enteignen",
+      "channel": "topic:global",
+      "timestamp": "2020-01-22 02:25:00.000000",
+      "text": "Hallo liebe Tester*innen! Es gibt eine neue Version mit der Nummer 0.5.3.\nDies sind die neuen Bugfixes:\n- (manche) Chat-Benachrichtigungen nennen zugehörige Aktion\n- nur noch eine Benachrichtigungen pro Chat gleichzeitig\n- nur noch 100 Aktionen gleichzeitig anzeigen\n- neuer Hinweis-Dialog zu Benachrichtigungseinstellungen auf Profil-Seite\n- besser aufgelöste Logos\n- Fix bei Kiez-Auswahl auf mittlerer Zoomstufe\n- vergrößerndes Eingebefeld des Chats\n- GUI-Fehlerbehebung im Feedback-Formular",
+      "color": 4292668415, // DWE hellblau für App-Info
+      // "color": 4294960680, // DWE mittelgelb für Deutsche Wohnen & Co. Enteignen
+      "sender_name": "App-Info",
       "user_id": -1
     },
-    "notification": {"title": "Titel", "body": "Meldung"}
+    "notification": {"title": "Wieder eine neue Version! (0.5.3)", "body": "> Tipp mich an um die News-Seite zu zeigen! <."}
   };
 
   final response = await post(
-      'http://127.0.0.1:18080/service/push/topic/global',
+      'https://dwe.idash.org/service/push/topic/global',
       headers: {
         "Authorization":
             "Basic MzpmZjdhOGI2Yi1lMTVhLTRmZWUtOWY3OS1kY2EwNmZkNmM4ODM=",
