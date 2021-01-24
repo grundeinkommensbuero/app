3e3559d4b72360221f812b07c28e800ce6ba25fe
kybernetik <chemo@mailbox.org>
2021-01-24 17:08:25 +0100

    erlaube doppelte Benutzernamne

--- sammel_server/src/main/java/de/kybernetik/rest/BenutzerRestResource.kt
+++ sammel_server/src/main/java/de/kybernetik/rest/BenutzerRestResource.kt
@@ -50,14 +50,14 @@ open class BenutzerRestResource {
         }
 
          // Zum Vermeiden optisch ähnlicher Namen
-        login.user.name = login.user.name?.trim()
-
-        if (benutzer.name != null && dao.benutzernameExistiert(benutzer.name!!)) {
-            return Response
-                    .status(412)
-                    .entity(RestFehlermeldung("Benutzername ist bereits vergeben"))
-                    .build()
-        }
+//        login.user.name = login.user.name?.trim()
+//
+//        if (benutzer.name != null && dao.benutzernameExistiert(benutzer.name!!)) {
+//            return Response
+//                    .status(412)
+//                    .entity(RestFehlermeldung("Benutzername ist bereits vergeben"))
+//                    .build()
+//        }
 
         try {
             val benutzerAusDb = dao.legeNeuenBenutzerAn(benutzer.convertToBenutzer())
