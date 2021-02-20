262500fa3ff8925632e540848e98c387ef8321a2
kybernetik <chemo@mailbox.org>
2021-02-21 00:02:58 +0100

    besseres Beispiel für globale Push-Nachricht

--- sammel_app/tools/pushToTopic.dart
+++ sammel_app/tools/pushToTopic.dart
@@ -3,6 +3,36 @@ import 'dart:convert';
 import 'package:http/http.dart';
 
 Future<void> main() async {
+  var titel = "Neue Version 1.1";
+  var inhalt = '''Liebe Sammler*innen. Kurz vor dem offiziellen Sammel-Start gibt es noch einmal die neue App-Version 1.1
+Die Version beinhaltet kleinere Fehlerbehebungen und wichtige Features, die es nicht mehr in die Release-Version geschafft hatten.
+  
+Achtung: Die alte Version 1.0 wird nun nicht mehr richtig funktionieren, darum aktualisiert die App unbedingt über euren App-Store!
+  
+Neuerungen in der Version 1.1:
+  
+Neue Sprachen:
+• russisch
+• französisch
+
+Neue Features:
+• Bedienungsanleitung in Fragen&Antworten-Seite
+• Erinnerung für ausstehendes Feedback
+• Aktionsliste scrollt automatisch zu anstehenden Aktionen
+• Neue Aktionsarten: Plakatieren, Kundgebung
+• Neue Artikel auf Fragen&Antworten-Seite
+• App merkt sich Selbstbeschreibung
+• Hinweis bei vergangenen Aktionen
+
+Fehlerbehebungen:
+• Fehler behoben durch den die App Chat-Nachrichten wieder 'vergessen' hat
+• Termin-Darum nun veränderbar
+• sinnvolle Dauer-Angabe bei Aktionen unter einer Stunde
+
+Viel Spaß und Erfolg! Fehler und Anregungen könnt ihr weiterhin gerne an app@dwenteignen.de melden
+
+Das App-Team :)''';
+
   final body = {
     "data": {
       "type": "TopicChatMessage",
