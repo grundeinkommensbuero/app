80c4a375b77c0f4e86c95761ac768fbae3b47f6b
kybernetik <chemo@mailbox.org>
2021-02-21 00:03:42 +0100

    besseres Beispiel für globale Push-Nachricht

--- sammel_app/tools/pushToTopic.dart
+++ sammel_app/tools/pushToTopic.dart
@@ -37,18 +37,18 @@ Das App-Team :)''';
     "data": {
       "type": "TopicChatMessage",
       "channel": "topic:global",
-      "timestamp": "2020-01-22 02:25:00.000000",
-      "text": "Hallo liebe Tester*innen! Es gibt eine neue Version mit der Nummer 0.5.3.\nDies sind die neuen Bugfixes:\n- (manche) Chat-Benachrichtigungen nennen zugehörige Aktion\n- nur noch eine Benachrichtigungen pro Chat gleichzeitig\n- nur noch 100 Aktionen gleichzeitig anzeigen\n- neuer Hinweis-Dialog zu Benachrichtigungseinstellungen auf Profil-Seite\n- besser aufgelöste Logos\n- Fix bei Kiez-Auswahl auf mittlerer Zoomstufe\n- vergrößerndes Eingebefeld des Chats\n- GUI-Fehlerbehebung im Feedback-Formular",
+      "timestamp": "2021-02-20 23:00:00.000000",
+      "text": '$titel\n\n$inhalt',
       "color": 4292668415, // DWE hellblau für App-Info
       // "color": 4294960680, // DWE mittelgelb für Deutsche Wohnen & Co. Enteignen
       "sender_name": "App-Info",
       "user_id": -1
     },
-    "notification": {"title": "Wieder eine neue Version! (0.5.3)", "body": "> Tipp mich an um die News-Seite zu zeigen! <."}
+    "notification": {"title": titel, "body": inhalt}
   };
 
   final response = await post(
-      'https://dwe.idash.org/service/push/topic/global',
+      'https://dwe.idash.org/service/push/topic/test-global',
       headers: {
         "Authorization":
             "Basic MzpmZjdhOGI2Yi1lMTVhLTRmZWUtOWY3OS1kY2EwNmZkNmM4ODM=",
