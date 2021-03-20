fa9ab82eb2cf95dd44a04deee11682d8a301daf2
kybernetik <chemo@mailbox.org>
2021-03-20 22:40:04 +0100

    Abhängigkeiten updaten #238 - Kryptografie auf neue Syntax angepasst

--- sammel_app/lib/shared/Crypter.dart
+++ sammel_app/lib/shared/Crypter.dart
@@ -1,14 +1,16 @@
 import 'dart:convert';
+
 import 'package:cryptography/cryptography.dart';
 
 import '../Provisioning.dart';
 
+final mac = MacAlgorithm.empty;
+final algorithm = AesCbc.with256bits(macAlgorithm: mac);
 final SecretKey key =
     SecretKey(base64.decode('vue8NkTYyN1e2OoHGcapLZWiCTC+13Eqk9gXBSq4azc='));
-
 final SecretKey pKey = SecretKey(base64.decode(prodKey));
 
-Map<String, dynamic> decrypt(dynamic data) {
+Future<Map<String, dynamic>> decrypt(dynamic data) async {
   if (data['encrypted'] == 'Base64') {
     if (data['payload'] == null) return {***REMOVED***
     var decodiert = jsonDecode(utf8.decode(base64Decode(data['payload'])));
@@ -17,18 +19,20 @@ Map<String, dynamic> decrypt(dynamic data) {
   ***REMOVED***
   if (data['encrypted'] == 'AES') {
     if (data['payload'] == null) return {***REMOVED***
-    final nonce = Nonce(base64.decode(data['nonce']));
-    final decrypted = aesCbc.decryptSync(base64.decode(data['payload']),
-        secretKey: key, nonce: nonce);
+    SecretBox secretBox = SecretBox(
+        base64.decode(data['payload']),
+        nonce: base64.decode(data['nonce']), mac: Mac.empty);
+    final decrypted = await algorithm.decrypt(secretBox, secretKey: key);
     final decoded = jsonDecode(utf8.decode(decrypted));
     print('Verschlüsselte Push-Nachricht (test): $decoded');
     return decoded;
   ***REMOVED***
   if (data['encrypted'] == 'AES_PROD') {
     if (data['payload'] == null) return {***REMOVED***
-    final nonce = Nonce(base64.decode(data['nonce']));
-    final decrypted = aesCbc.decryptSync(base64.decode(data['payload']),
-        secretKey: pKey, nonce: nonce);
+    SecretBox secretBox = SecretBox(
+        base64.decode(data['payload']),
+        nonce: base64.decode(data['nonce']), mac: Mac.empty);
+    final decrypted = await algorithm.decrypt(secretBox, secretKey: pKey);
     final decoded = jsonDecode(utf8.decode(decrypted));
     print('Verschlüsselte Push-Nachricht (prod): $decoded');
     return decoded;
