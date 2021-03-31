6b2c158c8503bc6005308fbae0168f28db85c3c8
Simon Huesken <shuesken@posteo.net>
2021-03-31 16:18:08 +0200

    train storage service

--- sammel_app/test/shared/mocks.trainer.dart
+++ sammel_app/test/shared/mocks.trainer.dart
@@ -9,19 +9,26 @@ import '../shared/mocks.costumized.dart';
 import '../shared/mocks.mocks.dart';
 import 'TestdatenVorrat.dart';
 
-trainStammdatenService(MockStammdatenService mock) {
+MockStammdatenService trainStammdatenService(MockStammdatenService mock) {
   when(mock.kieze).thenAnswer((_) =>
       Future.value([ffAlleeNord(), tempVorstadt(), plaenterwald()].toSet()));
   when(mock.regionen).thenAnswer(
       (_) => Future.value([fhainOst(), kreuzbergSued(), koepenick1()].toSet()));
   when(mock.ortsteile).thenAnswer((_) =>
       Future.value([friedrichshain(), kreuzberg(), koepenick()].toSet()));
+  return mock;
 ***REMOVED***
 
 MockUserService trainUserService(MockUserService mock) {
   when(mock.user).thenAnswer((_) => Stream.value(karl()));
   when(mock.userHeaders)
       .thenAnswer((_) async => {'Authorization': 'userCreds'***REMOVED***);
+  return mock;
+***REMOVED***
+
+MockStorageService trainStorageService(MockStorageService mock) {
+  when(mock.loadAllStoredEvaluations())
+      .thenAnswer((_) => Future.value(List.empty()));
   return mock;
 ***REMOVED***
 
