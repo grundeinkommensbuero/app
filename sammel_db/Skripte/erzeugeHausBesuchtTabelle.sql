c82cfc3cf13268c474295b1ea0dc5db86656cb7e
kybernetik <chemo@mailbox.org>
2021-07-17 16:49:32 +0200

    Hausbesuch-Datenformat #254

--- sammel_db/Skripte/erzeugeHausBesuchtTabelle.sql
+++ sammel_db/Skripte/erzeugeHausBesuchtTabelle.sql
@@ -1,4 +1,4 @@
-create table HausBesucht
+create table BesuchteHaeuser
 (
     id        int auto_increment        primary key,
     latitude  double       not null,
@@ -6,5 +6,5 @@ create table HausBesucht
     adresse   varchar(120) not null,
     hausteil  varchar(120) null,
     datum     datetime     not null,
-    user_id   int          not null,
+    user_id   int          not null
 );
