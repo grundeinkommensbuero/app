ff4b5df52afa178169e14a81f7692b86387f579b
kybernetik <chemo@mailbox.org>
2021-07-18 18:56:53 +0200

    Plakate-Datentyp (Closes #248)

---
+++ sammel_db/Skripte/fuellePlakateTabelle.sql
@@ -0,0 +1,3 @@
+INSERT INTO Plakate (id, latitude, longitude, adresse, user_id) VALUES (1, 52.4722460, 13.3277830, '12161, Friedrich-Wilhelm-Platz 57', 11);
+INSERT INTO Plakate (id, latitude, longitude, adresse, user_id) VALUES (2, 52.47102, 13.3282, '12161, Bundesallee 76', 12);
+INSERT INTO Plakate (id, latitude, longitude, adresse, user_id) VALUES (3, 52.4709, 13.32744, '12161, Goßlerstraße 29', 11)
