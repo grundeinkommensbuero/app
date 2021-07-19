b8cff574774a27bafc10ea94c0c026dd117601f6
kybernetik <chemo@mailbox.org>
2021-07-20 01:53:47 +0200

    Plakate in App eintragen #247 Datenmodell

---
+++ sammel_app/lib/services/PlakateService.dart
@@ -0,0 +1,10 @@
+import 'package:sammel_app/model/Placcard.dart';
+import 'package:sammel_app/services/BackendService.dart';
+
+
+abstract class AbstractPlakateService extends BackendService {
+  AbstractPlakateService(Backend backend): super(backend);
+
+  Future<List<Placcard>> loadPlaccards();
+
+***REMOVED***
