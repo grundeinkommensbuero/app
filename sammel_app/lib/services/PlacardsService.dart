62092ee82eecd128c0eca95999eabdb5168b8b64
kybernetik <chemo@mailbox.org>
2021-07-21 12:03:42 +0200

    Plakate werden mit Aktualisieren neu geladen

--- sammel_app/lib/services/PlacardsService.dart
+++ sammel_app/lib/services/PlacardsService.dart
@@ -47,6 +47,7 @@ class PlacardsService extends AbstractPlacardsService {
   @override
   Future<List<Placard>> loadPlacards() async {
     HttpClientResponseBody response;
+    print('### Lade Plakate');
     try {
       response = await get('service/plakate');
     } catch (e, s) {
