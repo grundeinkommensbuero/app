4923b5162937dd6b2dbc92ef848fcc82a4a834b3
kybernetik <chemo@mailbox.org>
2021-07-24 01:45:19 +0200

    Shape und OsmId an Besuchtes Haus

--- sammel_db/Skripte/erzeugeBesuchteHaeuserTabelle.sql
+++ sammel_db/Skripte/erzeugeBesuchteHaeuserTabelle.sql
@@ -5,6 +5,8 @@ create table BesuchteHaeuser
     longitude double       not null,
     adresse   varchar(120) not null,
     hausteil  varchar(120) null,
+    polygon   text         null,
+    osm_id     varchar(20)  null,
     datum     datetime     not null,
     user_id   int          not null
 );
