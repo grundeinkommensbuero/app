cce62f7df3662584d57581cc68b8395327d76b76
kybernetik <chemo@mailbox.org>
2021-08-08 14:55:23 +0200

    Vorbehalte-Funktion #263 - Datenbank-Tabelle und Test-Daten

--- sammel_db/Skripte/erzeugeVorbehalteTabelle.sql
+++ sammel_db/Skripte/erzeugeVorbehalteTabelle.sql
@@ -1,7 +1,8 @@
 create table Vorbehalte
 (
     id          int auto_increment        primary key,
+    vorbehalte  text         not null,
+    benutzer    int          not null,
     datum       datetime     not null,
-    ort         varchar(120) not null,
-    vorbehalte  text         not null
+    ort         varchar(120) not null
 );
--- sammel_db/Skripte/fuelleVorbehalteTabelle.sql
+++ sammel_db/Skripte/fuelleVorbehalteTabelle.sql
@@ -1,3 +1,3 @@
-INSERT INTO Vorbehalte (id, datum, ort, vorbehalte) VALUES (1, '2021-08-08 00:00:00', 'Frankfurter Allee Nord', 'Neubau (2), Kosten');
-INSERT INTO Vorbehalte (id, datum, ort, vorbehalte) VALUES (2, '2021-08-09 00:00:00', 'Tempelhofer Vorstadt', 'Entschädigung, DDR');
-INSERT INTO Vorbehalte (id, datum, ort, vorbehalte) VALUES (3, '2021-08-10 00:00:00', 'Alexanderplatz', 'Haushalt, Bauen, Kommunismus');
+INSERT INTO Vorbehalte (id, vorbehalte, benutzer, datum, ort) VALUES (1, 'Neubau (2), Kosten', 11, '2021-08-08 00:00:00', 'Frankfurter Allee Nord');
+INSERT INTO Vorbehalte (id, vorbehalte, benutzer, datum, ort) VALUES (2, 'Entschädigung, DDR', 12, '2021-08-09 00:00:00', 'Tempelhofer Vorstadt');
+INSERT INTO Vorbehalte (id, vorbehalte, benutzer, datum, ort) VALUES (3, 'Haushalt, Bauen, Kommunismus', 11, '2021-08-10 00:00:00', 'Alexanderplatz');
