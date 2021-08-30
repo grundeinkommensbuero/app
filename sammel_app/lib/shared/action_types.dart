2561973808849046d1c46a6e694110309bfb0302
Valioesi <Vali_Schagerl@web.de>
2021-08-30 14:41:08 +0200

    New xbge icons

--- sammel_app/lib/shared/action_types.dart
+++ sammel_app/lib/shared/action_types.dart
@@ -1,11 +1,5 @@
 List<String> moeglicheTypen = [
   'Sammeln',
   'Infoveranstaltung',
-  'Workshop',
   'Plakatieren',
-  'Kundgebung',
-  'Haustürgespräche',
-  'Straßengespräche & Flyern',
-  'Kiezfest',
-  'Plakate aufhängen',
 ];
